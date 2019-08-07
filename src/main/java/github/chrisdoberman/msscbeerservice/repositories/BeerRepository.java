31af4777a84e943f9c58078c610933d9705ad23f
chrisdoberman <chris.herb@gmail.com>
2019-08-06 22:53:49 -0400

    added beer repository. closes #3

---
+++ src/main/java/github/chrisdoberman/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package github.chrisdoberman.msscbeerservice.repositories;
+
+import github.chrisdoberman.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
