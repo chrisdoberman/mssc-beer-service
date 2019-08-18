02c05aaec381aee0521c5c40e55b8bdfa0741aa8
chrisdoberman <chris.herb@gmail.com>
2019-08-18 16:24:37 -0400

    implemented controller - closes #10

---
+++ src/main/java/github/chrisdoberman/msscbeerservice/services/BeerServiceImpl.java
@@ -0,0 +1,41 @@
+package github.chrisdoberman.msscbeerservice.services;
+
+import github.chrisdoberman.msscbeerservice.domain.Beer;
+import github.chrisdoberman.msscbeerservice.repositories.BeerRepository;
+import github.chrisdoberman.msscbeerservice.web.controller.NotFoundException;
+import github.chrisdoberman.msscbeerservice.web.mappers.BeerMapper;
+import github.chrisdoberman.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class BeerServiceImpl implements BeerService {
+
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
+    @Override
+    public BeerDto getById(UUID beerId) {
+        return beerMapper.beerToBeerDto(beerRepository.findById(beerId).orElseThrow(NotFoundException::new));
+    }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return beerMapper.beerToBeerDto(beerRepository.save(beerMapper.beerDtoToBeer(beerDto)));
+    }
+
+    @Override
+    public BeerDto updateBeer(UUID beerId, BeerDto beerDto) {
+        Beer beer = beerRepository.findById(beerId).orElseThrow(NotFoundException::new);
+
+        beer.setBeerName(beerDto.getBeerName());
+        beer.setBeerStyle(beerDto.getBeerStyle().name());
+        beer.setPrice(beerDto.getPrice());
+        beer.setUpc(beerDto.getUpc());
+
+        return beerMapper.beerToBeerDto(beerRepository.save(beer));
+    }
+}
