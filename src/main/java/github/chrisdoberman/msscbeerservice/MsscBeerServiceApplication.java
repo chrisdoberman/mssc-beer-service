1cd97211a07d24004f12cb3c3580f21d26d10a6c
chrisdoberman <chris.herb@gmail.com>
2019-08-31 12:55:03 -0400

    added mysql init scripts, fixed some domain hibernate annotations

--- src/main/java/github/chrisdoberman/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/github/chrisdoberman/msscbeerservice/MsscBeerServiceApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsscBeerServiceApplication {
-    //test
+
     public static void main(String[] args) {
         SpringApplication.run(MsscBeerServiceApplication.class, args);
     }
-
 }
