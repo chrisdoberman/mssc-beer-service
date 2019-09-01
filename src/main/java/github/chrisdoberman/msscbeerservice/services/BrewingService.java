4f43a3ffff81491de81eb10a564de78e433dde1e
chrisdoberman <chris.herb@gmail.com>
2019-08-31 23:03:20 -0400

    BrewingService to trigger brewing closes #16

--- src/main/java/github/chrisdoberman/msscbeerservice/services/BrewingService.java
+++ src/main/java/github/chrisdoberman/msscbeerservice/services/BrewingService.java
@@ -1,10 +1,15 @@
 package github.chrisdoberman.msscbeerservice.services;
 
+import github.chrisdoberman.msscbeerservice.config.JmsConfig;
 import github.chrisdoberman.msscbeerservice.domain.Beer;
+import github.chrisdoberman.msscbeerservice.events.BeerEvent;
 import github.chrisdoberman.msscbeerservice.repositories.BeerRepository;
 import github.chrisdoberman.msscbeerservice.services.inventory.BeerInventoryService;
+import github.chrisdoberman.msscbeerservice.web.mappers.BeerMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,8 +21,11 @@ public class BrewingService {
 
     private final BeerRepository beerRepository;
     private final BeerInventoryService beerInventoryService;
+    private final JmsTemplate jmsTemplate;
+    private final BeerMapper beerMapper;
 
 
+    @Scheduled(fixedRate = 5000) // every 5 seconds
     public void checkForLowInventory() {
         List<Beer>  beers = beerRepository.findAll();
 
@@ -26,8 +34,9 @@ public class BrewingService {
             log.debug("Min on hand: {}", beer.getMinOnHand());
             log.debug("Inventory is: {}", invQOH);
 
-            if (beer.getMinOnHand() <= invQOH) {
-
+            // i think this is a bug, should be <=
+            if (beer.getMinOnHand() >= invQOH) {
+                jmsTemplate.convertAndSend(JmsConfig.BREWING_REQUEST_QUEUE, new BeerEvent(beerMapper.beerToBeerDto(beer)));
             }
         });
     }
